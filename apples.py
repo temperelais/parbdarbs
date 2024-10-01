f086125cedbd908c029932117ae888b54614aca2
temperelais <149486977+temperelais@users.noreply.github.com>
2024-10-01 11:06:16 +0300

    iesākts projekts
    s

---
+++ apples.py
@@ -0,0 +1,15 @@
+class Harvest:
+    def __init__(self, VegOrFruit, name, howMuch):
+        self.VegOrFruit = VegOrFruit
+        self.name = name
+        self.howMuch = howMuch
+
+    def ateSome(self, howManyEaten)
+        self.howMuch-=howManyEaten
+
+class Apple(Harvest):
+    def __init__(self, name, howMuch,appleBreed):
+        super().__init__("fruit", name, howMuch,appleBreed)
+        self.appleBreed = appleBreed
+
+        
