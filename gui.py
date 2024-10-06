e7b7fdf9a68fdd6c409551ca3de5f71036ff0ee5
temperelais <ntempelmanis@edu.riga.lv>
2024-10-06 21:40:17 +0300

    ievietoti labeli un entries visam, ko vecmamma prasa

--- gui.py
+++ gui.py
@@ -3,43 +3,69 @@ import tkinter as tk
 from tkinter import ttk, END
 from tkinter import *
 
-# programas izskats
+allHarvests = []
 
+# Program appearance
 root = tk.Tk()
 root.title("Auglu un darzenu skaititajs")
-root.geometry("500x300")
+root.geometry("800x800")
 
+# Create the frame
 frame = ttk.Frame(root)
+frame.pack()
 
-# field options
-
-options = {'padx':5,'pady': 5}
+# Field options
+options = {'padx': 5, 'pady': 5}
 
 # vegOrFruit label
-vegOrFruit_label = ttk.Label(frame, text="Dārzenis vai Auglis?")
-vegOrFruit_label.grid(column=2, row=2, sticky="e", **options)
+vegOrFruit_label = ttk.Label(frame, text="Objekts ir Dārzenis vai Auglis?")
+vegOrFruit_label.grid(column=0, row=0, sticky="E", **options)
 
-# vegOrFruit Radiobox
+# vegOrFruit Radiobox entry
+def sel():
+    return str(VegOrFruit.get())
 
-var = tk.StringVar()
-def resultOfRadBox():
-    print(var.get())
+VegOrFruit = tk.StringVar()
+VegOrFruitRadiobox1 = ttk.Radiobutton(frame, text="Auglis", variable=VegOrFruit, value="Fruit", command=sel())
+VegOrFruitRadiobox2 = ttk.Radiobutton(frame, text="Dārzenis", variable=VegOrFruit, value="vegetable", command=sel())
 
-theRadButtons =(tk.Radiobutton(root, text="Produkts ir auglis", variable=var, value="fruit", command=resultOfRadBox),
-                tk.Radiobutton(root, text="Produkts ir dārzenis", variable=var, value="vegetable", command=resultOfRadBox))
-count =1
-for i in theRadButtons:
-    i.grid(column=1,row=count,**options)
-    count+=1
+VegOrFruitRadiobox1.grid(column=0, row=1, sticky="E", **options)
+VegOrFruitRadiobox2.grid(column=0, row=2, sticky="E", **options)
 
-# 
+# howMuch label
+howMuchLabel = ttk.Label(frame, text="Cik daudz ir prece(kilogramos)?")
+howMuchLabel.grid(column=1, row=0, sticky="E", **options)
+
+#howMuch entry
+howMuch=tk.StringVar()
+howMuchEntry = ttk.Entry(frame, textvariable=howMuch)
+howMuchEntry.grid(column=1, row=1, **options)
+howMuchEntry.focus()
+
+#name label
+nameLabel = ttk.Label(frame, text="Kāds ražas nosaukums?")
+nameLabel.grid(column=2, row=0, sticky="E", **options)
+
+# name entry
+name = tk.StringVar()
+nameEntry = ttk.Entry(frame, textvariable=name)
+nameEntry.grid(column=2, row=1, **options)
+
+# breed label
+breedLabel = ttk.Label(frame, text="Kāda ir sķirne?(ja ābols)")
+breedLabel.grid(column=3, row=0, sticky="E", **options)
+
+# breed entry
+breed = tk.StringVar()
+breedEntry = ttk.Entry(frame, textvariable=breed)
+breedEntry.grid(column=3, row=1, **options)
+
+# create Object button
+def createHarvestButton():
+    daudzums = howMuch.get()
+    whatType = VegOrFruit.get()
+    whatName = name.get()
 
 
-
-
-
-
-
-# start the loop
+# Start the loop
 root.mainloop()
-
